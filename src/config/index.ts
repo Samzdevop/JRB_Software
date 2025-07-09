2f9d8c1c11756f704f79b9e80cb0b71fd2aea72e
samuel <samzdevop@gmail.com>
2025-07-09 12:53:03 +0100

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
 
 console.log('Environment loaded:', process.env.JWT_SECRET ? 'Yes' : 'No');
 
+dotenv.config();
 
 export const config = {
     PORT: process.env.PORT || 5000,
