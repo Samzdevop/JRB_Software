0ea08ab760e64a3def09547c9512987fb88f3011
samuel <samzdevop@gmail.com>
2025-07-10 19:49:30 +0100

    deployment

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -2,20 +2,20 @@ import { PrismaClient } from '@prisma/client';
 
 
 const prisma = new PrismaClient({
-    datasources: { db: { url: process.env.DATABASE_URL } },
-        log: ['warn', 'error'],
-        transactionOptions: {
-            maxWait: 5000,
-            timeout: 10000
-        }
-    // datasources: {
-    //     db: {
-    //         url: process.env.DATABASE_URL + "&connection_limit=5&pool_timeout=10"
+    // datasources: { db: { url: process.env.DATABASE_URL } },
+    //     log: ['warn', 'error'],
+    //     transactionOptions: {
+    //         maxWait: 5000,
+    //         timeout: 10000
     //     }
-    // },
-    //  log: process.env.NODE_ENV === 'development' 
-    // ? ['query', 'error', 'warn'] 
-    // : ['error', 'warn']
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL + "&connection_limit=5&pool_timeout=10"
+        }
+    },
+     log: process.env.NODE_ENV === 'development' 
+    ? ['query', 'error', 'warn'] 
+    : ['error', 'warn']
 });
 
 export default prisma;
